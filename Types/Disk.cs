eb5b0f21b969f1f296c0db5942d5980ad2e86817
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-07-01 01:19:56 +0200

    addajda

---
+++ Types/Disk.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ServerInfoAPI.Types
+{
+    public struct Disk
+    {
+        public string Name { get; set; }
+        public string[] Partitions { get; set; }
+
+        public int IOPS { get; set; }
+
+        public int Latency { get; set; }
+        public int? Temperature { get; set; }
+    }
+}
