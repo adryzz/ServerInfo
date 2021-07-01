da6ccd7d56d52a6a253a687ee141d327db4bbb2b
adryzz <46694241+adryzz@users.noreply.github.com>
2021-07-01 21:56:38 +0200

    hmm yeah better

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -21,7 +21,6 @@ namespace ServerInfo.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
